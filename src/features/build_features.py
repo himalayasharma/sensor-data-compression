0dd4314f9eea6086f25e9d1f5e40f9d4280e6974
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-07-18 01:53:16 -0400

    Remove imports of unused packages

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-import os, wget, zipfile
+import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
 import logging
 from pathlib import Path
