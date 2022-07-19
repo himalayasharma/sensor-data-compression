33da40a0c59fe30cda00a25f64bb5fecb3a0d29a
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-07-19 03:54:38 -0400

    Add random forest and svc accuracy

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,29 +4,51 @@ import pickle
 from random import random
 import pandas as pd
 from pathlib import Path
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def get_knn_accuracy(dr_data_dict, y_train, y_test):
 
-    from sklearn.neighbors import KNeighborsClassifier
     knn_acc_dict = dict()
     for key, item in dr_data_dict.items():
-        knn = KNeighborsClassifier(n_neighbors=7)
-        knn.fit(item[0], y_train)
-        knn_acc_dict[key] = knn.score(item[1], y_test)
+        knn = KNeighborsClassifier(n_neighbors=3)
+        knn.fit(item[1], y_train)
+        knn_acc_dict[key] = knn.score(item[2], y_test)
         knn_acc = pd.DataFrame.from_dict(knn_acc_dict, orient='index', columns=['KNN(n_neighbors=7) accuracy'])
     return knn_acc
 
 def get_decision_tree_accuracy(dr_data_dict, y_train, y_test):
 
-    from sklearn.tree import DecisionTreeClassifier
     decision_tree_acc_dict = dict()
     for key, item in dr_data_dict.items():
         decision_tree = DecisionTreeClassifier(random_state=42)
-        decision_tree.fit(item[0], y_train)
-        decision_tree_acc_dict[key] = decision_tree.score(item[1], y_test)
+        decision_tree.fit(item[1], y_train)
+        decision_tree_acc_dict[key] = decision_tree.score(item[2], y_test)
         decision_tree_acc = pd.DataFrame.from_dict(decision_tree_acc_dict, orient='index', columns=['Decision tree accuracy'])
     return decision_tree_acc
+
+def get_random_forest_accuracy(dr_data_dict, y_train, y_test):
+
+    random_forest_acc_dict = dict()
+    for key, item in dr_data_dict.items():
+        random_forest = RandomForestClassifier(random_state=42, n_jobs=-1)
+        random_forest.fit(item[1], y_train)
+        random_forest_acc_dict[key] = random_forest.score(item[2], y_test)
+        random_forest_acc = pd.DataFrame.from_dict(random_forest_acc_dict, orient='index', columns=['Random forest accuracy'])
+    return random_forest_acc
+
+def get_svc_accuracy(dr_data_dict, y_train, y_test):
+
+    svc_acc_dict = dict()
+    for key, item in dr_data_dict.items():
+        svc = LinearSVC(random_state=42)
+        svc.fit(item[1], y_train)
+        svc_acc_dict[key] = svc.score(item[2], y_test)
+        svc_acc = pd.DataFrame.from_dict(svc_acc_dict, orient='index', columns=['SVC accuracy'])
+    return svc_acc
 
 def main(base_dir):
 
@@ -49,10 +71,10 @@ def main(base_dir):
     logger.info('loaded test data')
 
     # -------------- Load data dictionary -----------------
-    with open(os.path.join(processed_data_dir, 'dr_data_dict'), 'rb') as file_pi:
+    with open(os.path.join(processed_data_dir, 'dr_data_dict_modelling'), 'rb') as file_pi:
         dr_data_dict = pickle.load(file_pi)
     # Add original data
-    dr_data_dict["Original"] = (X_train, X_test)
+    dr_data_dict["Original"] = (533, X_train, X_test)
 
     # -------------- Load statistics summary ----------------- 
     with open(os.path.join(processed_data_dir, 'statistics_summary'), 'rb') as file_pi:
@@ -68,6 +90,16 @@ def main(base_dir):
     decision_tree_acc = get_decision_tree_accuracy(dr_data_dict, y_train, y_test)
     statistics_summary = pd.concat([statistics_summary, decision_tree_acc], axis=1)
     logger.info('added decision tree accuracy to statistics summary')
+
+   # -------------- Add Random forest accuracy ----------------- 
+    random_forest_acc = get_random_forest_accuracy(dr_data_dict, y_train, y_test)
+    statistics_summary = pd.concat([statistics_summary, random_forest_acc], axis=1)
+    logger.info('added random forest accuracy to statistics summary')
+
+   # -------------- Add SVC accuracy ----------------- 
+    svc_acc = get_svc_accuracy(dr_data_dict, y_train, y_test)
+    statistics_summary = pd.concat([statistics_summary, svc_acc], axis=1)
+    logger.info('added svc accuracy to statistics summary')
 
     print(statistics_summary)
 
